8f11a8b94ac753bb86854a89be08c193c4975cef
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-10-13 17:28:16 +0200

    innerhtml fix

--- js/master.js
+++ js/master.js
@@ -1,21 +1,21 @@
 gameGridDOMElement = document.querySelector('.game-grid')
-console.log(gameGridDOMElement)
+// console.log(gameGridDOMElement)
 
 startGameBtnDOMElement = document.getElementById('start-game')
-console.log(startGameBtnDOMElement)
+// console.log(startGameBtnDOMElement)
 
 startGameBtnDOMElement.addEventListener('click', function (){
 
     gameGridDOMElement.innerHTML = ''
 
-    for (i = 0; i < 100 ; i++) {
+    for (let i = 0; i < 100 ; i++) {
 
-        n = i + 1
+        let n = i + 1
         // console.log(n)
 
         const htmlCell = `<div class="cell">${n}</div>`
 
-        gameGridDOMElement = gameGridDOMElement.innerHTML + htmlCell
-        console.log(gameGridDOMElement)
+        gameGridDOMElement.innerHTML = gameGridDOMElement.innerHTML + htmlCell
+
     }
 })
